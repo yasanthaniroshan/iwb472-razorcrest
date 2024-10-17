c6cab9438825bedd2ede046b8a58804a83ad648f
RashmikaDushan <kbrashmikadushan@gmail.com>
2024-10-17 23:21:23 +0530

    features sections done

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,13 +1,5 @@
-import { ReactNode } from "react";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Code, Layers, Zap } from "lucide-react";
+
 import NavBar from "./components/NavBar.tsx";
 import Footer from "./components/Footer.tsx";
 import "./homepage.css";
@@ -20,7 +12,7 @@ export default function HomePage() {
         <section className="relative w-screen py-12 md:py-24 lg:py-32 xl:py-48 h-full z-0">
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+              <h1 className="text-3xl text-blue-500 font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
                 Welcome to InvoTrak
               </h1>
               <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
@@ -51,40 +43,74 @@ export default function HomePage() {
             />
           </div>
         </section>
-        <section className="relative w-screen py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800 z-10 shadow-[0_-20px_30px_-15px_rgba(0,0,0,0.1)]">
-          <div className="px-4 md:px-6">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-12">
-              Features
+        <section className="relative w-screen sm:h-[20vh] md:h-[50vh] lg:h-[75vh] py-12 md:py-24 lg:py-32 bg-blue-500 dark:bg-gray-800 z-10 shadow-[0_0_100px_-15px_rgba(0,0,0,0.3)] text-center flex justify-center items-center">
+          <div className="flex flex-col">
+            <h1 className="text-3xl font-bold sm:text-3xl md:text-4xl lg:text-5xl/none lg:mr-20 text-white mb-8">
+              Automated Billing Emails
+            </h1>
+            <h2 className="text-lg sm:text-lg md:text-2xl lg:text-3xl font-light text-blue-50 text-white tracking-tighter sm:text-3xl mx-32">
+              Simplifies billing by automating invoice generation, sending
+              emails, and tracking payments in real-time. It's an easy-to-use
+              platform designed to streamline your invoicing process, saving you
+              time and helping you stay on top of your finances.
             </h2>
-            <div className="grid gap-6 lg:grid-cols-3">
-              <FeatureCard
-                icon={<Zap className="w-8 h-15 mb-4" />}
-                title="Lightning Fast"
-                description="Experience unparalleled speed and performance."
-              />
-              <FeatureCard
-                icon={<Layers className="w-8 h-15 mb-4" />}
-                title="Highly Customizable"
-                description="Tailor the app to fit your unique needs and preferences."
-              />
-              <FeatureCard
-                icon={<Code className="w-8 h-15 mb-4" />}
-                title="Developer Friendly"
-                description="Built with developers in mind, offering powerful APIs and integrations."
-              />
+          </div>
+        </section>
+        <section className="relative pt-24 md:pt-32 lg:pt-42 z-100 h-[60vh]">
+          <div className="text-center mb-32">
+            <h1 className="text-3xl text-gray-400 font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+              Features
+            </h1>
+          </div>
+          <div className="flex flex-row w-screen">
+            <img
+              src={"/svg1.svg"}
+              alt="svg1"
+              className="sm:w-[18rem] md:w-[24rem] lg:w-[36rem] xl:w-[46rem] sm:pl-20 lg:pl-32 feature"
+            />
+            <div className="space-y-2 ml-20 content-center text-left w-full ">
+              <h1 className="text-lg font-bold sm:text-xl md:text-2xl  text-teal-400 lg:text-4xl/none lg:mr-20">
+                Automated Billing Emails
+              </h1>
+              <p className="text-gray-500 md:text-md dark:text-gray-400 mr-20">
+                Automatically create and send billing emails, ensuring timely
+                invoices and payment reminders.
+              </p>
             </div>
           </div>
         </section>
-        <section className="w-screen py-12 md:py-24 lg:py-32">
+        <section className="pt-12 md:pt-24 lg:pt-52 z-100  h-[60vh]">
           <div className="flex flex-row w-screen">
-            <img src={"/svg1.svg"} alt="svg1" className="w-[38rem] pl-32" />
-            <div className="space-y-2">
-              <h1 className="text-xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                Feature 1
+            <div className="space-y-2 mr-20 content-center text-right w-full">
+              <h1 className="text-lg font-bold sm:text-xl md:text-2xl  text-amber-400 lg:text-4xl/none lg:ml-20">
+                Customizable Invoice Templates
               </h1>
-              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                The revolutionary app that simplifies your workflow and boosts
-                productivity.
+              <p className="text-gray-500 md:text-md dark:text-gray-400 lg:ml-20">
+                Personalize your invoices with customizable templates, including
+                your logo and payment details.
+              </p>
+            </div>
+            <img
+              src={"/svg2.svg"}
+              alt="svg2"
+              className="sm:w-[18rem] md:w-[24rem] lg:w-[36rem] xl:w-[46rem] sm:pr-20 lg:pr-32 feature"
+            />
+          </div>
+        </section>
+        <section className="pt-12 md:pt-24 lg:pt-72 h-[60vh] mb-24 md:mb-36 lg:mb-60">
+          <div className="flex flex-row w-screen">
+            <img
+              src={"/svg1.svg"}
+              alt="svg1"
+              className="sm:w-[18rem] md:w-[24rem] lg:w-[36rem] xl:w-[46rem] sm:pl-20 lg:pl-32 feature z-100"
+            />
+            <div className="space-y-2 ml-20 content-center w-full">
+              <h1 className="text-lg font-bold sm:text-xl text-green-400 md:text-2xl lg:text-4xl/none lg:mr-20">
+                Real-Time Payment Tracking
+              </h1>
+              <p className="text-gray-500 md:text-md dark:text-gray-400 mr-20">
+                Instantly track payments with real-time updates for completed,
+                pending, or overdue invoices.
               </p>
             </div>
           </div>
@@ -92,25 +118,5 @@ export default function HomePage() {
       </main>
       <Footer />
     </div>
-  );
-}
-
-interface FeatureCardProps {
-  icon: ReactNode;
-  title: string;
-  description: string;
-}
-
-function FeatureCard({ icon, title, description }: FeatureCardProps) {
-  return (
-    <Card>
-      <CardHeader>
-        {icon}
-        <CardTitle>{title}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <CardDescription>{description}</CardDescription>
-      </CardContent>
-    </Card>
   );
 }
